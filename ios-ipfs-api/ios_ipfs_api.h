1a9b6fd68bc32892d3600cb3be7dcb242d01f4cc
이현종 <F000000066@wemakeprice.com>
2018-08-27 11:08:20 +0900

    Add carthage.

---
+++ ios-ipfs-api/ios_ipfs_api.h
@@ -0,0 +1,19 @@
+//
+//  ios_ipfs_api.h
+//  ios-ipfs-api
+//
+//  Created by we on 2018. 8. 27..
+//  Copyright © 2018년 jaylee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios_ipfs_api.
+FOUNDATION_EXPORT double ios_ipfs_apiVersionNumber;
+
+//! Project version string for ios_ipfs_api.
+FOUNDATION_EXPORT const unsigned char ios_ipfs_apiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_ipfs_api/PublicHeader.h>
+
+
