1736bc1dbddc680f64e945678c9e9cc59b1eb1d7
이현종 <F000000066@wemakeprice.com>
2018-08-27 10:57:46 +0900

    Add carthage.

---
+++ Example/IpfsApi/IpfsApi.h
@@ -0,0 +1,19 @@
+//
+//  IpfsApi.h
+//  IpfsApi
+//
+//  Created by we on 2018. 8. 24..
+//  Copyright © 2018년 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IpfsApi.
+FOUNDATION_EXPORT double IpfsApiVersionNumber;
+
+//! Project version string for IpfsApi.
+FOUNDATION_EXPORT const unsigned char IpfsApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IpfsApi/PublicHeader.h>
+
+
